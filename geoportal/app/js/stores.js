f99bee2e6660fe0aa97865bb3b73f7b11656abbd
Alejandro Perez <sakacel2@gmail.com>
2024-05-24 18:39:20 +0000

    coññit select stores

--- geoportal/app/js/stores.js
+++ geoportal/app/js/stores.js
@@ -29,32 +29,35 @@ export function insert_store(){
     });
 }
 
+
 export function select_store(){
-    let gid=document.getElementById('form-buildings-gid').value;
-    axios.get(URL_API + '/appdesweb/building_select_by_gid/',
+    let gid=document.getElementById('form-stores-gid').value;
+    axios.get(URL_API + '/appdesweb/select_store_by_gid/',
     {params:{
             gid:gid
         }
-    }, {withCredentials: true})
+    })
     .then(function (response) {
     // handle success
             console.log(response);
             if (response.data.data.length > 0){
-                document.getElementById("form-buildings-gid").value=response.data.data[0].gid;
-                document.getElementById("form-buildings-descripcion").value=response.data.data[0].descripcion;
-                document.getElementById("form-buildings-geomWkt").value=response.data.data[0].st_astext;
-                document.getElementById("form-buildings-area").value=response.data.data[0].area;    
+                document.getElementById('form-stores-gid').value=response.data.data[0].gid.value;
+                document.getElementById('form-stores-client_segment_id').value=response.data.data[0].client_segment_id.value;
+                document.getElementById('form-stores-store_name').value=response.data.data[0].store_name.value;
+                document.getElementById('form-stores-store_description').value=response.data.data[0].description.value;
+                document.getElementById('form-stores-geomWkt').value=response.data.data[0].st_astext;   
             }else{
-                document.getElementById("form-buildings-descripcion").value="";
-                document.getElementById("form-buildings-geomWkt").value="";
-                document.getElementById("form-buildings-area").value=""; 
+                document.getElementById('form-stores-client_segment_id').value=""; 
+                document.getElementById('form-stores-store_name').value=""; 
+                document.getElementById('form-stores-store_description').value=""; 
+                document.getElementById('form-stores-geomWkt').value=""; 
             }
-            document.getElementById("form-buildings-message").innerHTML=response.data.message;
+            document.getElementById("form-stores-message").innerHTML=response.data.message;
         })
         .catch(function (error) {
             // handle error
             console.log(error);
-            document.getElementById("form-buildings-message").innerHTML=error.message;
+            document.getElementById("form-stores-message").innerHTML=error.message;
         })
         .finally(function () {
             // always executed
@@ -66,6 +69,7 @@ export function update_store(){
     let descripcion=document.getElementById('form-buildings-descripcion').value;
     let geomWkt=document.getElementById('form-buildings-geomWkt').value;
 
+
     axios.post(URL_API + '/appdesweb/building_insert/',
     {descripcion:descripcion,geomWkt:geomWkt}, {withCredentials: true})
     .then(function (response) {
@@ -85,25 +89,60 @@ export function update_store(){
             console.log('Finally')
     });
 }
+
+/* 
+
+export function delete_store(){
+    let gid=document.getElementById('form-stores-gid').value;
+    axios.post(URL_API + '/appdesweb/delete_store_by_gid/',
+    {params:{
+        gid:gid
+    }
+        })
+    .then(function (response) {
+    // handle success
+            console.log(response);
+            document.getElementById("form-stores-gid").value=response.data.data[0].gid;
+            //document.getElementById("form-stores-client_segment_id").value=response.data.data[0].client_segment_id;
+            //document.getElementById("form-stores-store_name").value=response.data.data[0].store_name;
+            //document.getElementById("form-stores-store_description").value=response.data.data[0].store_description;
+            //document.getElementById("form-stores-geomWkt").value=response.data.data[0].st_astext;
+            //document.getElementById("form-buildings-area").value=response.data.data[0].area;
+    
+            document.getElementById("form-stores-message").innerHTML=response.data.message;
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            document.getElementById("form-stores-message").innerHTML=error.message;
+        })
+        .finally(function () {
+            // always executed
+            console.log('Finally')
+    });
+}
+ */
 
 
 export function delete_store(){
-    let descripcion=document.getElementById('form-buildings-descripcion').value;
-    let geomWkt=document.getElementById('form-buildings-geomWkt').value;
+    let gid=document.getElementById('form-stores-gid').value;
+    //let client_segment_id=document.getElementById('form-stores-client_segment_id').value;
+    //let store_name=document.getElementById('form-stores-store_name').value;
+    //let store_description=document.getElementById('form-stores-store_description').value;
+    //let geomWkt=document.getElementById('form-stores-geomWkt').value;
 
-    axios.post(URL_API + '/appdesweb/building_insert/',
-    {descripcion:descripcion,geomWkt:geomWkt}, {withCredentials: true})
+    axios.post(URL_API + '/appdesweb/delete_store_by_gid/',{gid:gid})
     .then(function (response) {
     // handle success
             console.log(response);
-            document.getElementById("form-buildings-gid").value=response.data.data[0].gid;
-            document.getElementById("form-buildings-area").value=response.data.data[0].area;
-            document.getElementById("form-buildings-message").innerHTML=response.data.message;
+            document.getElementById("form-stores-gid").value=response.data.data[0].gid;
+            //document.getElementById("form-stores-area").value=response.data.data[0].area;
+            document.getElementById("form-stores-message").innerHTML=response.data.message;
         })
         .catch(function (error) {
             // handle error
             console.log(error);
-            document.getElementById("form-buildings-message").innerHTML=error.message;
+            document.getElementById("form-stores-message").innerHTML=error.message;
         })
         .finally(function () {
             // always executed
@@ -111,5 +150,4 @@ export function delete_store(){
     });
 }
 
-
-
+    
