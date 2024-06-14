fcad2059e6f490c8fff01db023e7c4ff9376d2aa
Alejandro Perez <sakacel2@gmail.com>
2024-06-14 18:00:07 +0000

    all worink in one page without map

--- js/registerEvents.js
+++ js/registerEvents.js
@@ -7,12 +7,12 @@ import {login, logout} from './login';
 export function registerEvents(){
     document.getElementById('button-login').addEventListener('click',login);
     document.getElementById('button-logout').addEventListener('click',logout);
-
 }
 
+
 window.onload = function(){
-    //registerEventsClients();
-    //registerEventsStores();
-    //registerEventsStreets();
+    registerEventsClients();
+    registerEventsStores();
+    registerEventsStreets();
     registerEvents();
 }
