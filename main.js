aea033d438ef379cda8d455c73e77e7e6435fc5e
Alejandro Perez <sakacel2@gmail.com>
2024-06-15 10:03:36 +0000

    commit

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import 'ol-layerswitcher/dist/ol-layerswitcher.css'
 import "./js/registerEvents";
 import { helloWord } from "./js/helloWorld";
 import {Map, View} from 'ol';
-import {mapMain} from './js/map/mapMain';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+//import {mapMain} from './js/map/mapMain';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
 //import {setMap_MAIN} from 'js/settings';
 //Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
@@ -14,3 +14,16 @@ import Alert from 'bootstrap/js/dist/alert';
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
+
+export const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  });
